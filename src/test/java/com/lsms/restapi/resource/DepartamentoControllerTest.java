8bab694c9493394eb671b5eeb0fa4b51af4f6326
Danilo Tovar <danilo.tovar@udea.edu.co>
2024-04-22 00:50:08 -0500

    Correcciones de SonarCloud - Coverage

---
+++ src/test/java/com/lsms/restapi/resource/DepartamentoControllerTest.java
@@ -0,0 +1,96 @@
+package com.lsms.restapi.resource;
+
+import com.lsms.restapi.dto.DepartamentoDTO;
+import com.lsms.restapi.persistence.Departamento;
+import com.lsms.restapi.service.DepartamentoService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+
+public class DepartamentoControllerTest {
+    @Mock
+    private DepartamentoService departamentoService;
+
+    @InjectMocks
+    private DepartamentoController departamentoController;
+
+    private DepartamentoDTO departamentoDTO;
+    private Departamento departamento;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this.departamentoService);
+        departamento = new Departamento();
+        departamento.setId(10L);
+        departamento.setNombre("Antioquia");
+        departamentoDTO = new DepartamentoDTO();
+        departamentoDTO.setId(departamento.getId());
+        departamentoDTO.setNombre(departamento.getNombre());
+    }
+
+    @Test
+    void getAll(){
+        when(departamentoService.getAll()).thenReturn(Collections.singletonList(departamento));
+        assertNotNull(departamentoController.getAll());
+    }
+
+    @Test
+    void getById() {
+        when(departamentoService.getById(eq(10L))).thenReturn(departamento);
+        Departamento departamentoObtenido = departamentoController.getById(10L);
+
+        verify(departamentoService, times(1)).getById(eq(10L));
+        assertNotNull(departamentoObtenido);
+        assertEquals(departamento.getNombre(), departamentoObtenido.getNombre());
+
+        System.out.println(departamento);
+    }
+
+    @Test
+    void add() {
+        when(departamentoService.add(any(Departamento.class))).thenReturn(departamento);
+
+        Departamento savedDepartamento = departamentoController.add(departamentoDTO);
+
+        assertNotNull(savedDepartamento, "El departamento guardado no debería ser nulo");
+        assertEquals(departamento.getNombre(), savedDepartamento.getNombre(), "El nombre del departamento guardado no coincide");
+
+        verify(departamentoService, times(1)).add(eq(departamento));
+        System.out.println("El departamento registrado es: "+departamento);
+    }
+
+    @Test
+    void update() {
+        when(departamentoService.add(any(Departamento.class))).thenReturn(departamento);
+
+        Departamento savedDepartamento = departamentoController.getById(departamento.getId());
+
+        assertNotNull(savedDepartamento, "El departamento guardado no debería ser nulo");
+        assertEquals(departamento.getNombre(), savedDepartamento.getNombre(), "El nombre del modificado guardado no coincide");
+        assertEquals(departamento.getId(), savedDepartamento.getId(), "El ID del departamento modificado no coincide");
+
+        verify(departamentoService, times(1)).add(eq(departamento));
+        System.out.println("El departamento registrado es: "+departamento);
+    }
+
+    @Test
+    void delete() {
+        when(departamentoService.getById(eq(10L))).thenReturn(new Departamento());
+        departamentoController.delete(10L);
+
+        verify(departamentoService, times(1)).delete(eq(10L));
+        verify(departamentoService, never()).getById(eq(10L));
+        verify(departamentoService, times(1)).delete(eq(10L));
+        System.out.println("Id del departamento eliminado es:"+10L);
+    }
+}
---
+++ src/test/java/com/lsms/restapi/service/DepartamentoServiceTest.java
@@ -0,0 +1,94 @@
+package com.lsms.restapi.service;
+
+import com.lsms.restapi.persistence.Departamento;
+import com.lsms.restapi.persistence.DepartamentoRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class DepartamentoServiceTest {
+
+    @Mock
+    private DepartamentoRepository departamentoRepository;
+    @InjectMocks
+    private DepartamentoService departamentoService;
+
+    private Departamento departamento;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this.departamentoRepository);
+        departamento = new Departamento();
+        departamento.setId(10L);
+        departamento.setNombre("Antioquia");
+    }
+
+    @Test
+    void getAll() {
+        when(departamentoRepository.findAll()).thenReturn(Collections.singletonList(departamento));
+        assertNotNull(departamentoService.getAll()); //Traerá los valores que no sean nulos
+        System.out.println(departamento);
+    }
+
+    @Test
+    void add() {
+        when(departamentoRepository.save(any(Departamento.class))).thenReturn(departamento);
+
+        Departamento savedDepartamento = departamentoService.add(departamento);
+
+        assertNotNull(savedDepartamento, "El departamento guardado no debería ser nulo");
+        assertEquals("Antioquia", savedDepartamento.getNombre(), "El nombre del departamento guardado no coincide");
+
+        verify(departamentoRepository, times(1)).save(eq(departamento));
+        System.out.println("El departamento registrado es: "+departamento);
+    }
+
+    @Test
+    void getById() {
+        when(departamentoRepository.findById(eq(10L))).thenReturn(Optional.of(departamento));
+        Departamento departamentoObtenido = departamentoService.getById(10L);
+
+        verify(departamentoRepository, times(1)).findById(eq(10L));
+        assertNotNull(departamentoObtenido);
+        assertEquals("Antioquia", departamentoObtenido.getNombre());
+
+        System.out.println(departamento);
+    }
+
+    @Test
+    void update() {
+        when(departamentoRepository.save(any(Departamento.class))).thenReturn(departamento);
+
+        Departamento savedDepartamento = departamentoService.getById(departamento.getId());
+
+        assertNotNull(savedDepartamento, "El departamento guardado no debería ser nulo");
+        assertEquals(departamento.getNombre(), savedDepartamento.getNombre(), "El nombre del modificado guardado no coincide");
+        assertEquals(departamento.getId(), savedDepartamento.getId(), "El ID del departamento modificado no coincide");
+
+        verify(departamentoRepository, times(1)).save(eq(departamento));
+        System.out.println("El departamento registrado es: "+departamento);
+    }
+
+    @Test
+    void delete() {
+
+        when(departamentoRepository.findById(eq(10L))).thenReturn(Optional.of(new Departamento()));
+        departamentoService.delete(10L);
+
+        verify(departamentoRepository, times(1)).deleteById(eq(10L));
+        verify(departamentoRepository, never()).findById(eq(10L));
+        verify(departamentoRepository, times(1)).deleteById(eq(10L));
+        System.out.println("Id del departamento eliminado es:"+10L);
+
+    }
+}
