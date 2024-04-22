e48d344092d89523d22fd2edd2f5846a5a53ecfb
Danilo Tovar <danilo.tovar@udea.edu.co>
2024-04-22 01:24:59 -0500

    Correcciones de SonarCloud - Coverage

--- src/test/java/com/lsms/restapi/RestapiApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.lsms.restapi;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class RestapiApplicationTests {
-
-}
