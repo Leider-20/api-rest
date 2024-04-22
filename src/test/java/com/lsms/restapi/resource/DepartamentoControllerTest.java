d206bdcd7b824d3cc5acd93a23ac719d3227875d
Danilo Tovar <danilo.tovar@udea.edu.co>
2024-04-22 01:43:17 -0500

    Correcciones de SonarCloud - Coverage

--- src/test/java/com/lsms/restapi/resource/DepartamentoControllerTest.java
+++ src/test/java/com/lsms/restapi/resource/DepartamentoControllerTest.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-
+//
 public class DepartamentoControllerTest {
     @Mock
     private DepartamentoService departamentoService;
