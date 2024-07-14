162f2a6f456fd6299aa0b3c410c123c9b30143df
LeiderF <leider.caicedo@udea.edu.co>
2024-07-14 18:59:01 -0500

    Added class description

--- src/main/java/com/lsms/restapi/config/WebConfig.java
+++ src/main/java/com/lsms/restapi/config/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    /** Esta clase es para habilitar la política de seguridad del navegador llamada CORS (Cross-Origin Resource Sharing).
+    /** Esta clase es para habilitar solicitudes relacionadas a la política de seguridad del navegador llamada CORS (Cross-Origin Resource Sharing).
     Esta política bloquea las solicitudes que se realizan desde un origen distinto al que sirve el recurso solicitado,
      a menos que el servidor permita explícitamente dichas solicitudes.*/
 
