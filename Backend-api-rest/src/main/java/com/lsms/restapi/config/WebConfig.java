b8a62dc3530732d1b9d5812cdb9e6509924039c6
LeiderF <leider.caicedo@udea.edu.co>
2025-07-02 00:30:16 -0500

    feat: modify the WebConfig class

--- Backend-api-rest/src/main/java/com/lsms/restapi/config/WebConfig.java
+++ Backend-api-rest/src/main/java/com/lsms/restapi/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://192.168.1.101:5501")
+                .allowedOrigins("http://192.168.1.100:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
